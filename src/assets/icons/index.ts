0cdb78ac689f1763d20b0a99b8b7d04d8ac5a3c4
KimDaegyu <ddk3207@naver.com>
2025-08-07 17:17:41 +0900

    style: 아이콘 추가

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,39 @@
+import Home from './icon_home.svg?react'
+import Matching from './icon_matching.svg?react'
+import Dish from './icon_dish.svg?react'
+import Meeting from './icon_meeting.svg?react'
+import Comment from './icon_comment.svg?react'
+import Repply from './icon_repply.svg?react'
+import Search from './icon_search.svg?react'
+import Send from './icon_send.svg?react'
+import Alarm from './icon_alarm.svg?react'
+import Back from './icon_back.svg?react'
+import Kebab from './icon_kebab.svg?react'
+import ProfileDefault from './icon_profile_default.svg?react'
+import Delete from './icon_delete.svg?react'
+import Call from './icon_call.svg?react'
+import Heart from './icon_heart.svg?react'
+import Location from './icon_location.svg?react'
+import EmptyView from './icon_emptyview.svg?react'
+import EmptyViewNoti from './icon_emptyview_noti.svg?react'
+
+export {
+    Home,
+    Matching,
+    Dish,
+    Meeting,
+    Comment,
+    Repply,
+    Search,
+    Send,
+    Alarm,
+    Back,
+    Kebab,
+    ProfileDefault,
+    Delete,
+    Call,
+    Heart,
+    Location,
+    EmptyView,
+    EmptyViewNoti
+}
