7daedc7ebfae2ca2083d209eaf54966012332a71
KimDaegyu <ddk3207@naver.com>
2025-08-19 04:59:33 +0900

    feat: 앱 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import './main.css'
-import HomePage from './pages/HomePage'
-import GPSPage from './pages/GPSPage'
-import CameraPage from './pages/CameraPage'
-import NotificationPage from './pages/NotificationPage'
-import PushNotificationPage from './pages/PushNotificationPage'
-import WebSocketPage from './pages/WebSocketPage'
-import Layout from './components/Layout'
+import {
+    TestPage,
+    GPSPage,
+    NotificationPage,
+    PushNotificationPage,
+    LocationSelectionPage,
+    HomePage,
+    ProfilePage,
+    RestaurantPage,
+    MatchingPage,
+    UploadPage,
+    WaitingPage,
+    SearchRestaurantPage,
+    SocketTestPage,
+    MeetingPage,
+    MenuPage,
+    MenuExcludePage,
+    LocationVotePage,
+    FinishPage,
+    TimePage,
+    DatePage,
+    SendInvitationPage,
+    ReadInvitationPage,
+    CouponStoragePage,
+    NotiStoragePage,
+    CommentPage,
+    ProfileEditPage,
+    BobCheckHistoryPage,
+    AllergicMenuPage,
+    PreferMenuPage,
+    MakeProfilePage
+} from '@/pages'
+import { Layout, OnboardingLayout } from '@/components'
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -17,24 +42,138 @@ const queryClient = new QueryClient({
         }
     }
 })
-
 function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <Router>
-                <Layout>
-                    <Routes>
+                <Routes>
+                    <Route element={<Layout />}>
+                        {/* 네비게이션 페이지 */}
                         <Route
                             path="/"
                             element={<HomePage />}
                         />
                         <Route
-                            path="/gps"
-                            element={<GPSPage />}
+                            path="/profile"
+                            element={<ProfilePage />}
                         />
                         <Route
-                            path="/camera"
-                            element={<CameraPage />}
+                            path="/meeting"
+                            element={<MeetingPage />}
+                        />
+                        <Route
+                            path="/matching"
+                            element={<MatchingPage />}
+                        />
+
+                        {/* 홈 페이지 */}
+                        <Route
+                            path="/comment/:postId"
+                            element={<CommentPage />}
+                        />
+                        <Route
+                            path="/search-restaurant"
+                            element={<SearchRestaurantPage />}
+                        />
+                        <Route
+                            path="/noti"
+                            element={<NotiStoragePage />}
+                        />
+                        <Route
+                            path="/upload"
+                            element={<UploadPage />}
+                        />
+                        <Route
+                            path="/restaurant"
+                            element={<RestaurantPage />}
+                        />
+
+                        {/* 매치 페이지 */}
+                        <Route
+                            path="/send-invitation"
+                            element={<SendInvitationPage />}
+                        />
+                        <Route
+                            path="/read-invitation"
+                            element={<ReadInvitationPage />}
+                        />
+
+                        {/* 프로필 페이지 */}
+                        <Route
+                            path="/coupon"
+                            element={<CouponStoragePage />}
+                        />
+                        <Route
+                            path="/profile-edit"
+                            element={<ProfileEditPage />}
+                        />
+                        <Route
+                            path="/bob-check-history"
+                            element={<BobCheckHistoryPage />}
+                        />
+
+                        {/* 회원가입 온보딩 페이지 */}
+                        <Route
+                            path="/allergic-menu"
+                            element={<AllergicMenuPage />}
+                        />
+                        <Route
+                            path="/prefer-menu"
+                            element={<PreferMenuPage />}
+                        />
+                        <Route
+                            path="/make-profile"
+                            element={<MakeProfilePage />}
+                        />
+
+                        {/* 매칭 온보딩 페이지 */}
+                        <Route element={<OnboardingLayout />}>
+                            <Route
+                                path="/:matchType/waiting/:roomId"
+                                element={<WaitingPage />}
+                            />
+                            <Route
+                                path="/:matchType/location/:roomId"
+                                element={<LocationSelectionPage />}
+                            />
+                            <Route
+                                path="/:matchType/menu/:roomId"
+                                element={<MenuPage />}
+                            />
+                            <Route
+                                path="/:matchType/restaurant/:roomId"
+                                element={<RestaurantPage />}
+                            />
+                            <Route
+                                path="/:matchType/exclude-menu/:roomId"
+                                element={<MenuExcludePage />}
+                            />
+                            <Route
+                                path="/:matchType/location-vote/:roomId"
+                                element={<LocationVotePage />}
+                            />
+                            <Route
+                                path="/:matchType/finish/:roomId"
+                                element={<FinishPage />}
+                            />
+                            <Route
+                                path="/:matchType/time/:roomId"
+                                element={<TimePage />}
+                            />
+                            <Route
+                                path="/:matchType/date/:roomId"
+                                element={<DatePage />}
+                            />
+                            <Route
+                                path="/:matchType/socket-test/:roomId"
+                                element={<SocketTestPage />}
+                            />
+                        </Route>
+
+                        {/* 테스트 페이지 */}
+                        <Route
+                            path="/gps"
+                            element={<GPSPage />}
                         />
                         <Route
                             path="/notifications"
@@ -45,11 +184,11 @@ function App() {
                             element={<PushNotificationPage />}
                         />
                         <Route
-                            path="/websocket"
-                            element={<WebSocketPage />}
+                            path="/test"
+                            element={<TestPage />}
                         />
-                    </Routes>
-                </Layout>
+                    </Route>
+                </Routes>
             </Router>
         </QueryClientProvider>
     )
