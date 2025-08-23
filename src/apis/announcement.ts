f5d26c0560d664efe9668c0071299b61c2e515c8
KimDaegyu <ddk3207@naver.com>
2025-08-23 10:42:44 +0900

    chore: api url 변경

--- src/apis/announcement.ts
+++ src/apis/announcement.ts
@@ -4,34 +4,43 @@ import { BaseResponse, PostRequest, PostResponse } from './dto'
 export const getAnnouncements = async (): Promise<
     BaseResponse<PostResponse[]>
 > => {
-    const res = await client.get('/posts')
+    const res = await client.get(`${import.meta.env.VITE_BASE_API_URL}/posts`)
     return res.data
 }
 
 export const postAnnouncement = async (
     data: PostRequest
 ): Promise<BaseResponse<void>> => {
-    const res = await client.post('/posts', data)
+    const res = await client.post(
+        `${import.meta.env.VITE_BASE_API_URL}/posts`,
+        data
+    )
     return res.data
 }
 
 export const closeAnnouncement = async (
     announcementId: number
 ): Promise<BaseResponse<void>> => {
-    const res = await client.put(`/posts/${announcementId}/close`)
+    const res = await client.put(
+        `${import.meta.env.VITE_BASE_API_URL}/posts/${announcementId}/close`
+    )
     return res.data
 }
 
 export const joinAnnouncement = async (
     announcementId: number
 ): Promise<BaseResponse<void>> => {
-    const res = await client.post(`/participate/${announcementId}`)
+    const res = await client.post(
+        `${import.meta.env.VITE_BASE_API_URL}/participate/${announcementId}`
+    )
     return res.data
 }
 
 export const subscribeAnnouncement = async (
     announcementId: number
 ): Promise<BaseResponse<void>> => {
-    const res = await client.post(`/subscribe/${announcementId}`)
+    const res = await client.post(
+        `${import.meta.env.VITE_BASE_API_URL}/subscribe/${announcementId}`
+    )
     return res.data
 }
