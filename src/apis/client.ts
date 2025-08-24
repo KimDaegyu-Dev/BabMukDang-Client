2d46e45fe54926f98f7957394e653d5852d726d6
KimDaegyu <ddk3207@naver.com>
2025-08-25 00:31:03 +0900

    fix: 로그인 쿠키 설정

--- src/apis/client.ts
+++ src/apis/client.ts
@@ -30,19 +30,16 @@ client.interceptors.response.use(
         if (error.response?.status === 401 && !originalRequest._retry) {
             originalRequest._retry = true
             const { logout, setTokens } = useAuthStore.getState()
-
             // 발급 시도 프로미스, 이미 발급 중이면 대기
             if (!isRefreshing) {
                 isRefreshing = true
-                refreshPromise = axios
-                    .post(`${import.meta.env.VITE_BASE_API_URL}/auth/refresh`)
+                refreshPromise = client
+                    .post(`${import.meta.env.VITE_BASE_API_URL}/auth/refresh`, {
+                        withCredentials: true
+                    })
                     .then(res => {
                         const { accessToken, refreshToken } = res.data
-                        console.log(
-                            accessToken,
-                            refreshToken,
-                            res.data
-                        )
+                        console.log(accessToken, refreshToken, res.data)
                         setTokens({ accessToken, refreshToken })
                         return accessToken
                     })
